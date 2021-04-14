86902d7ef44e8f99816718d871ef8be55f1c3327
gurezo <akihiko.kigure@gmail.com>
2021-04-14 17:22:08 +0900

    コメント追記

--- snippets.sh
+++ snippets.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # .vscode が無い時にフォルダ作成
+echo '.vscode ディレクトリ存在チェック・作成'
 if [ ! -e .vscode ]; then mkdir -p .vscode ; fi
 
 # カスタムスニペットをコピー
+echo '.vscode ディレクトリへカスタムスニペットをコピー'
 cp -f node_modules/custom-snippets/typescript.json ./.vscode
